7549820b1fb88e79fcf9f17b215c367f64cb8562
Elizaveta <lizasteplion@gmail.com>
2024-01-15 12:23:08 +0700

    fixed localstorage

--- src/components/todos/Todos.tsx
+++ src/components/todos/Todos.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
+import { useAppDispatch } from 'hooks/useAppDispatch';
 
 import { Todo, TodoField } from 'components';
 import { Button, Title, Wrapper } from 'assets/styles/globalStylesComponents';
@@ -10,18 +12,19 @@ import { saveLocalStorage, selectTodos } from 'store/todoSlice';
 import AssignmentSharpIcon from '@mui/icons-material/AssignmentSharp';
 import AssignmentTurnedInSharpIcon from '@mui/icons-material/AssignmentTurnedInSharp';
 import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
-import { useAppDispatch } from 'hooks/useAppDispatch';
 
 const Todos = () => {
     const dispatch = useAppDispatch();
     const myTodos = useAppSelector(selectTodos);
+    const storage = localStorage.getItem('todos');
+
     const total = myTodos.length;
     const completed = myTodos.filter(todo => todo.completed === true).length;
     const progress = Math.round((completed * 100) / total);
 
     const sortedTodos = [...myTodos].sort(a => (a.completed === false ? -1 : 1));
 
-    const [todos, setTodos] = useState(myTodos);
+    const [todos, setTodos] = useState(sortedTodos);
     const [statusAllTodo, setStatusAllTodo] = useState(false);
     const [statusActiveTodo, setStatusActiveTodo] = useState(false);
     const [statusCompletedTodo, setStatusCompletedTodo] = useState(false);
@@ -48,23 +51,20 @@ const Todos = () => {
     };
 
     useEffect(() => {
-        const storage = localStorage.getItem('todos');
-        return () => {
-            if (storage) {
-                dispatch(saveLocalStorage(JSON.parse(storage)));
-            }
-        };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    useEffect(() => {
         setTodos(sortedTodos);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [myTodos]);
 
     useEffect(() => {
         localStorage.setItem('todos', JSON.stringify(todos));
+        if (storage) {
+        }
     }, [todos]);
+
+    useEffect(() => {
+        if (storage) {
+            dispatch(saveLocalStorage(JSON.parse(storage)));
+        }
+    }, []);
 
     return (
         <Wrapper>
